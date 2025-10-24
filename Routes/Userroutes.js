07ddd7013aff997d1b5265e898980afc0f0de4e9
Sandy_0626 <santhoshm0626@gmail.com>
2025-10-24 11:13:09 +0530

    bcrypt pss

--- Routes/Userroutes.js
+++ Routes/Userroutes.js
@@ -1,15 +1,21 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router();
-const usermanagement = require("../Controller/Usercontroller");
-const Authmanagement = require("../Controller/Authcontroller");
-
-router.post("/adduser", usermanagement.adduser);
-router.get("/getuser", usermanagement.getuser);
-router.put("/updatedata/:id", usermanagement.updateuser);
-router.delete("/deleteuser/:id", usermanagement.deleteuser);
-router.get("/get/:id", usermanagement.getbyid)
 
 
-router.post("/register", Authmanagement.register);
-router.post("/login", Authmanagement.login);
-module.exports = router;
+const usermanagement = require("../Controller/Usercontroller")
+const Authmanagement = require("../Controller/Authcontroller")
+
+
+router.post("/adduser",usermanagement.adduser)
+router.get("/getdata",usermanagement.getuser)
+router.put("/updatedata/:id",usermanagement.updateuser)
+router.delete("/deleteuser/:id",usermanagement.deleteuser)
+router.get("/get/:id",usermanagement.getbyid)
+
+
+
+router.post("/register",Authmanagement.register)
+router.post("/login",Authmanagement.login)
+
+
+module.exports = router
